0f0f5e7ff3a996ee05838f0c0d081b3e53af0ac1
LeoPytho <108222261+LeoPytho@users.noreply.github.com>
2025-07-10 22:55:22 +0700

    Create index.mjs

---
+++ index.mjs
@@ -0,0 +1,54 @@
+import members from './src/features/jkt48/members.js'
+import memberDetail from './src/features/jkt48/memberDetail.js'
+import birthday from './src/features/jkt48/birthday.js'
+
+import events from './src/features/jkt48/events.js'
+import recent from './src/features/jkt48/recent.js'
+import replay from './src/features/jkt48/replay.js'
+import recentDetail from './src/features/jkt48/recentDetail.js'
+import live from './src/features/jkt48/live.js'
+import liveYoutube from './src/features/jkt48/liveYoutube.js'
+import liveIdn from './src/features/jkt48/liveIdn.js'
+import liveShowroom from './src/features/jkt48/liveShowroom.js'
+import youtube from './src/features/jkt48/youtube.js'
+
+import chatStream from './src/features/jkt48/chatStream.js'
+import chatStreamSR from './src/features/jkt48/chatStreamSr.js'
+
+import news from './src/features/jkt48/news.js'
+import newsDetail from './src/features/jkt48/newsDetail.js'
+
+import theater from './src/features/jkt48/theater.js'
+import theaterDetail from './src/features/jkt48/theaterDetail.js'
+
+import admin from './src/features/admin/admin.js'
+import database from './src/features/database/database.js'
+import check from './src/features/system/check.js'
+import gag from './src/features/gag/data.js'
+
+const jkt48Api = {
+  members,
+  memberDetail,
+  birthday,
+  events,
+  recent,
+  replay,
+  recentDetail,
+  live,
+  liveYoutube,
+  liveIdn,
+  liveShowroom,
+  youtube,
+  chatStream,
+  chatStreamSR,
+  news,
+  newsDetail,
+  theater,
+  theaterDetail,
+  admin,
+  database,
+  check,
+  gag
+}
+
+export default jkt48Api
